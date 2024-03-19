fc8570f9156560c92ae5c6642d1860fedf9ca5ce
mguittari <mattias.guittari@gmail.com>
2024-03-19 15:19:36 +0100

    Ivisaur ok

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,8 +1,27 @@
+const pokemonList = [
+  {
+    name: "Bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "Ivysaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/2.png",
+  },
+  { name: "Venusaur" },
+];
+
 function PokemonCard() {
+  const pokemon = pokemonList[0];
   return (
     <figure>
-      <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" />
-      <figcaption>Bulbasaur</figcaption>
+      {pokemon.imgSrc ? (
+        <img src={pokemon.imgSrc} alt={pokemon.name} />
+      ) : (
+        <figcaption>???</figcaption>
+      )}
+      <figcaption>{pokemon.name}</figcaption>
     </figure>
   );
 }
