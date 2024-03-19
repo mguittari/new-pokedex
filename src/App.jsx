ca0936d996ac2202026336d02a2a534e61a6046b
mguittari <mattias.guittari@gmail.com>
2024-03-19 22:42:05 +0100

    useState ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import PokemonCard from "./components/PokemonCard";
+import { useState } from "react";
 
 const pokemonList = [
   {
@@ -12,15 +13,50 @@ const pokemonList = [
     imgSrc:
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/2.png",
   },
-  { name: "Venusaur" },
+  {
+    name: "Venusaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/3.png",
+  },
+  {
+    name: "Charmander",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  },
+  {
+    name: "Charmeleon",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/5.png",
+  },
+  {
+    name: "Charizard",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/6.png",
+  },
 ];
 
 function App() {
+  const [pokemonIndex, setPokemonIndex] = useState(0);
+  const handleClickPrevious = () => {
+    setPokemonIndex(pokemonIndex - 1);
+  };
+  const handleClickNext = () => {
+    setPokemonIndex(pokemonIndex + 1);
+  };
   return (
     <div>
-      <PokemonCard pokemon={pokemonList[0]} />
+      <PokemonCard pokemon={pokemonList[pokemonIndex]} />
+
+      {pokemonIndex !== 0 && (
+        <button onClick={handleClickPrevious}>Previous</button>
+      )}
+      {pokemonIndex < pokemonList.length - 1 && (
+        <button onClick={handleClickNext}>Next</button>
+      )}
     </div>
   );
 }
 
 export default App;
+
+//   Rendu conditionnel pour les boutons : Ajoute du rendu conditionnel pour n'afficher le bouton "précédent" que si pokemonIndex > 0 (en français : "il y a bien un précédent"), et le bouton "suivant" que si pokemonIndex < pokemonList.length - 1 (en français : "il y a bien un suivant").
