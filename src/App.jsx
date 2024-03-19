2e6948eacb554aca36b6bd4efd04387a0f3e8611
mguittari <mattias.guittari@gmail.com>
2024-03-19 16:15:50 +0100

    props ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import "./App.css";
 import PokemonCard from "./components/PokemonCard";
 
+const pokemonList = [
+  {
+    name: "Bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "Ivysaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/2.png",
+  },
+  { name: "Venusaur" },
+];
+
 function App() {
   return (
     <div>
-      <PokemonCard />
+      <PokemonCard pokemon={pokemonList[0]} />
     </div>
   );
 }
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,19 +1,4 @@
-const pokemonList = [
-  {
-    name: "Bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "Ivysaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/2.png",
-  },
-  { name: "Venusaur" },
-];
-
-function PokemonCard() {
-  const pokemon = pokemonList[0];
+function PokemonCard({ pokemon }) {
   return (
     <figure>
       {pokemon.imgSrc ? (
