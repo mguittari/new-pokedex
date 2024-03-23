2f2a3102ec36897448ea01269e1dfbf94ba111f9
mguittari <mattias.guittari@gmail.com>
2024-03-23 18:33:17 +0100

    colors by type ok

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,18 +1,40 @@
 import PropTypes from "prop-types";
 
 function PokemonCard({ pokemon }) {
+  const getTypeColorClass = (type) => {
+    switch (type) {
+      case "water":
+        return "bg-gradient-to-b from-blue-700 to-cyan-500";
+      case "fire":
+        return "bg-gradient-to-b from-red-600 to-yellow-500";
+      case "grass":
+        return "bg-gradient-to-b from-green-600 to-lime-600";
+      case "electric":
+        return "bg-gradient-to-b from-yellow-300 to-yellow-500";
+      case "normal":
+        return "bg-gradient-to-b from-pink-500 to-rose-400";
+      case "poison":
+        return "bg-gradient-to-b from-fuchsia-700 to-purple-600";
+      default:
+        return "bg-gray-400";
+    }
+  };
   return (
-    <figure className="border border-black rounded-2xl max-w-md mb-2 mt-2 mx-auto bg-red-600 shadow-xl">
+    <figure
+      className={`rounded-2xl max-w-md mb-2 mt-2 mx-auto shadow-x pt-1 ${getTypeColorClass(
+        pokemon.type
+      )}`}
+    >
       {pokemon.imgSrc ? (
         <img
-          className="mx-auto border border-black rounded-2xl mt-12 w-80 h-80 bg-white shadow-xl"
+          className="mx-auto border rounded-2xl mt-12 w-80 h-80 bg-white shadow-x"
           src={pokemon.imgSrc}
           alt={pokemon.name}
         />
       ) : (
         <figcaption>???</figcaption>
       )}
-      <figcaption className="font-mono text-3xl mb-12 mt-12 text-white">
+      <figcaption className="font-mono text-3xl text-white mt-10 pb-10">
         {pokemon.name}
       </figcaption>
     </figure>
@@ -22,6 +44,7 @@ function PokemonCard({ pokemon }) {
 PokemonCard.propTypes = {
   pokemon: PropTypes.shape({
     name: PropTypes.string.isRequired,
+    type: PropTypes.string.isRequired,
     imgSrc: PropTypes.string.isRequired,
   }).isRequired,
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        x: "0px 0px 10px 2px rgba(0, 0, 0, 0.3)",
+      },
+      colors: {
+        bottle: "#025728",
+      },
+    },
   },
   plugins: [],
 };
